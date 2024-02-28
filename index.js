55d89d23ed03fd8f80e1c6ba4d00c48833562970
Manpreet Singh <singhman2005123@gmail.com>
2024-02-29 01:03:36 +0530

    first_commit

---
+++ index.js
@@ -0,0 +1,85 @@
+let { Client, GatewayIntentBits } = require("discord.js");
+const dataJson = require("./question.json");
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://127.0.0.1:27017/quiz-bot");
+
+const leaderSchema = new mongoose.Schema({
+  id: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  score: {
+    type: Number,
+    default: 0,
+  },
+  name: {
+    type: String,
+  },
+});
+
+const Leaderboard = mongoose.model("Leaderboard", leaderSchema);
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMessages,
+  ],
+});
+
+let questiondata = dataJson.results;
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+let count = 0;
+
+client.on("messageCreate", async (message) => {
+  let randomnumber = Math.floor(Math.random() * 49);
+  let id1 = message.author.id;
+  if (!message.author.bot) {
+    let score = await Leaderboard.findOne({ id: id1 });
+    if (!score) {
+      await Leaderboard.create({ id: id1, name: message.author.displayName });
+      console.log("New user added to the leaderboard.");
+    }
+  }
+
+  if (message.content.includes("quiz") && !message.author.bot) {
+    message.reply(questiondata[randomnumber].question).then(() => {
+      count = 0;
+    });
+  }
+
+  if (
+    message.author.id === id1 &&
+    !message.author.bot &&
+    count === 0 &&
+    (message.content.toLowerCase() === "true" ||
+      message.content.toLowerCase() === "false")
+  ) {
+    count++;
+    if (
+      questiondata[randomnumber].correct_answer.toLowerCase() ==
+      message.content.toLowerCase()
+    ) {
+      const updatedUser = await Leaderboard.findOneAndUpdate(
+        { id: id1 },
+        { $inc: { score: 1 } }
+      );
+
+      const scoreboard = await Leaderboard.findOne({ id: id1 });
+      console.log(questiondata[randomnumber].correct_answer.toLowerCase());
+      message.reply(`correct answer :) \n current score:${scoreboard.score}`);
+    } else {
+      const scoreboard = await Leaderboard.findOne({ id: id1 });
+      message.reply(`incorrect answer :( \n current score:${scoreboard.score}`);
+    }
+  }
+});
+
+client.login(
+  "MTIxMjQzNjMyOTQ0NzU1NTEzNA.G7Yiq4.1CQBirFy4pIO0pUnOlbANQh6oAgd4eQIyQK52g"
+);
